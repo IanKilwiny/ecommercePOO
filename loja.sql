e2e816bc3eb2ec1543e6efa48892eab64a9920ba
IanKilwiny <ribeiroian083@gmail.com>
2025-07-09 18:40:05 -0300

    Organizando o banco de dados

--- loja.sql
+++ loja.sql
@@ -1,55 +1,63 @@
+-- Criação do banco de dados e uso
 CREATE DATABASE loja;
 USE loja;
 
+-- Tabela de usuários
 CREATE TABLE `user` (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(10) NOT NULL
-);  
+);
 
+-- Tabela de produtos
 CREATE TABLE product (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     price DOUBLE NOT NULL,
     stock INT DEFAULT 0
 );
 
-CREATE TABLE cart_product (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    qtd INT DEFAULT 0,
-    fk_product INT,
-    
-	CONSTRAINT fk_cart_product_product FOREIGN KEY (fk_product) REFERENCES product(id)
-);
-
-CREATE TABLE purchase (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_cart_product INT,
+-- Tabela de carrinhos de compras (cada carrinho pertence a um usuário)
+CREATE TABLE cart (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     fk_user INT,
-    
-    CONSTRAINT fk_cart_user FOREIGN KEY (fk_user) REFERENCES `user`(id),
-    CONSTRAINT fk_cart_cartproduct FOREIGN KEY (fk_cart_product) REFERENCES cart_product(id)
+    CONSTRAINT fk_cart_user FOREIGN KEY (fk_user) REFERENCES `user`(id)
 );
 
+-- Tabela de relação entre carrinhos e produtos (quantidade de cada produto no carrinho)
+CREATE TABLE cart_product (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_cart INT,
+    fk_product INT,
+    qtd INT DEFAULT 0,
+    CONSTRAINT fk_cart_product_cart FOREIGN KEY (fk_cart) REFERENCES cart(id),
+    CONSTRAINT fk_cart_product_product FOREIGN KEY (fk_product) REFERENCES product(id)
+);
 
-INSERT INTO `user` (name, email) VALUES ("Ian Kilwiny", "iankilwiny@gmail.com");
+-- Inserção de dados de usuários
+INSERT INTO `user` (name, email, password) VALUES ("Ian Kilwiny", "iankilwiny@gmail.com", "12345");
 
+-- Inserção de dados de produtos
 INSERT INTO product (name, price, stock) VALUES ("Xbox Series S", 2300, 5);
 INSERT INTO product (name, price, stock) VALUES ("Iphone 12", 3200, 7);
 
-INSERT INTO cart_product (qtd, fk_product) VALUES (2, 1); -- Xbox
-INSERT INTO cart_product (qtd, fk_product) VALUES (3, 2); -- iPhone
+-- Inserção de carrinho para o usuário
+INSERT INTO cart (fk_user) VALUES (1);
 
+-- Inserção de produtos no carrinho, com a quantidade de cada
+INSERT INTO cart_product (fk_cart, fk_product, qtd) VALUES (1, 1, 2); -- Xbox Series S, 2 unidades
+INSERT INTO cart_product (fk_cart, fk_product, qtd) VALUES (1, 2, 3); -- Iphone 12, 3 unidades
 
-INSERT INTO purchase(fk_cart_product, fk_user) VALUES (1, 1);
-INSERT INTO purchase(fk_cart_product, fk_user) VALUES (2, 1);
-
-
+-- Consulta para ver os produtos no carrinho de um usuário
 SELECT 
     u.name AS usuario, 
-    pr.name AS produto
+    pr.name AS produto,
+    cp.qtd AS quantidade,
+    pr.price AS preco_unitario,
+    pr.price * cp.qtd AS total
 FROM `user` u
-INNER JOIN purchase ca ON ca.fk_user = u.id
-INNER JOIN cart_product cp ON ca.fk_cart_product = cp.id
-INNER JOIN product pr ON pr.id = cp.fk_product;
+INNER JOIN cart ca ON ca.fk_user = u.id
+INNER JOIN cart_product cp ON ca.id = cp.fk_cart
+INNER JOIN product pr ON pr.id = cp.fk_product
+WHERE u.id = 1;  -- Exemplo: Exibindo para o usuário com ID 1
